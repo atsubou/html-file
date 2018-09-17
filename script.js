fb7f939e7878b103d004971904e800bbbf86a13d
atsubou <atsu.soccer12@icloud.com>
2018-09-17 15:33:36 +0900

    js

--- script.js
+++ script.js
@@ -9,36 +9,6 @@ $(function(){
      $('#start-modal').fadeOut();
    });
 
-   $(function(){
-       setOperate();
-   });
-   function setOperate(){
-       setView();
-       var agent = navigator.userAgent;
-       if(agent.search(/iPhone/) != -1){
-           $("body").addClass("iphone");
-           window.onorientationchange = setView;
-       }else if(agent.search(/iPad/) != -1){
-           $("body").addClass("ipad");
-           window.onorientationchange = setView;
-       }else if(agent.search(/Android/) != -1){
-           $("body").addClass("android")
-           window.onresize = setView;
-       }else{
-           $("body").addClass("other"); 
-           window.onorientationchange = setView;
-       }
-   }
-   function setView(){
-       var orientation = window.orientation;
-       if(orientation === 0){
-           $("body").addClass("portrait")
-           $("body").removeClass("landscape");
-       }else{
-           $("body").addClass("landscape");
-           $("body").removeClass("portrait");
-       }
-   }
 
 
 });
